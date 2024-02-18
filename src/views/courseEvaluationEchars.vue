19ea09a84de9a694e9d323447c2315455e14561f
DESKTOP-9D52CCT\付建森 <1>
2024-02-18 23:00:46 +0800

    feat: echarts使用

--- src/views/courseEvaluationEchars.vue
+++ src/views/courseEvaluationEchars.vue
@@ -1,37 +1,57 @@
 <template>
-	<div class="container">
-		<div class="schart-box">
-			<div class="content-title">表格</div>
-		</div>
-		<div class="schart-box">
-			<div class="content-title">正负柱状图</div>
-			<div class="schart" id="echart_bar"></div>
-		</div>
-		<div class="schart-box">
-			<div class="content-title">饼状图</div>
-		</div>
-	</div>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">
+
+      </el-aside>
+      <el-main>
+        <div class="schart-box">
+          <div class="content-title">正负柱状图</div>
+          <div class="schart" id="echart_bar"></div>
+        </div>
+        <div class="schart-box">
+          <div class="content-title">饼状图</div>
+          <div class="schart" id="echart_pie"></div>
+        </div>
+      </el-main>
+    </el-container>
+  </div>
 </template>
 
 <script setup lang="ts" name="basecharts">
 import { useRoute } from 'vue-router';
 import * as echarts from 'echarts';
 import { getCourseEvaluation } from '../api/index';
+import { ElMessage } from 'element-plus';
 
 const route = useRoute();
 
 const evaluationId = route.params.evaluationId;
 
+const default_data = {
+  teachingContent: 0.28,
+  teachingMethod: 0.28,
+  curriculumManagement: 0.28,
+  courseAssessment: 0.28,
+  teachingAttitude: 0.28,
+  learningHarvest: 0.28
+}
+
+// 请求评价接口，获取数据
+try {
+  const res = await getCourseEvaluation(evaluationId.toString());
+
+} catch (error) {
+  ElMessage.error('数据获取失败');
+}
+
 var chartDom = document.getElementById('echart_bar');
 var myChart = echarts.init(chartDom);
 var option;
 
-const labelRight = {
-  position: 'top'
-};
 option = {
   title: {
-    text: 'Bar Chart with Negative Value'
+    text: '课程评价柱状图'
   },
   tooltip: {
     trigger: 'axis',
@@ -44,17 +64,18 @@ option = {
     bottom: 30
   },
   xAxis: {
-	type: 'category',
+    type: 'category',
     axisLine: { show: false },
     axisLabel: { show: false },
     axisTick: { show: false },
     splitLine: { show: false },
     data: [
-      'ten',
-      'nine',
-      'eight',
-      'seven',
-      'six'
+      '教学内容评价分',
+      '教学方法评价分',
+      '课程管理评价分',
+      '课程考核评价分',
+      '教学态度评价分',
+      '学习收获评价分'
     ]
   },
   yAxis: {
@@ -76,18 +97,62 @@ option = {
         formatter: '{b}'
       },
       data: [
-        { value: -0.07, label: labelRight },
-        { value: -0.09, label: labelRight },
-        0.2,
-        0.44,
-        { value: -0.23, label: labelRight },
-        0.08
+        default_data.teachingContent,
+        default_data.teachingMethod,
+        default_data.curriculumManagement,
+        default_data.courseAssessment,
+        default_data.teachingAttitude,
+        default_data.learningHarvest
       ]
     }
   ]
 };
 
 option && myChart.setOption(option);
+
+//饼状图
+var chartDom_pie = document.getElementById('echart_pie');
+var myChart_pie = echarts.init(chartDom_pie);
+var option_pie;
+
+option_pie = {
+  title: {
+    text: '课程评价饼图',
+    subtext: 'Fake Data',
+    left: 'center'
+  },
+  tooltip: {
+    trigger: 'item'
+  },
+  legend: {
+    orient: 'vertical',
+    left: 'left'
+  },
+  series: [
+    {
+      name: 'Access From',
+      type: 'pie',
+      radius: '50%',
+      data: [
+        { value: default_data.teachingContent, name: '教学内容评价分' },
+        { value: default_data.teachingMethod, name: '教学方法评价分' },
+        { value: default_data.curriculumManagement, name: '课程管理评价分' },
+        { value: default_data.courseAssessment, name: '课程考核评价分' },
+        { value: default_data.teachingAttitude, name: '教学态度评价分' },
+        { value: default_data.learningHarvest, name: '学习收获评价分' }
+      ],
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)'
+        }
+      }
+    }
+  ]
+};
+
+option_pie && myChart_pie.setOption(option_pie);
 
 
 </script>
